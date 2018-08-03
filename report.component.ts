be7dd98f0f028caad77bc1741eeecd8fd29192a4
Log <40469515+thenetlog@users.noreply.github.com>
2018-08-04 00:14:34 +0630

    Update report.component.ts

--- report.component.ts
+++ report.component.ts
@@ -1,71 +1,79 @@
-<h1>Search</h1>
+import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subject } from 'rxjs';
+import  { FormControl } from '@angular/forms';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { MatInput } from '@angular/material';
+//import { Moment } from 'moment';
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import {default as _rollupMoment, Moment} from 'moment';
 
-<div>
-  <mat-form-field>
-    <input matInput [matDatepicker]="dp" placeholder="Month and Year" [formControl]="date" #box>
-    <mat-datepicker-toggle matSuffix [for]="dp"></mat-datepicker-toggle>
-    <mat-datepicker #dp
-                    startView="multi-year"
-                    (yearSelected)="chosenYearHandler($event)"
-                    (monthSelected)="chosenMonthHandler($event, dp)"
-                    panelClass="example-month-picker">
-    </mat-datepicker>
-  </mat-form-field>
-</div>
-<p>{{ value }}</p>
-<button (click)="searchV(box.value)">Search</button>
+const moment:any = _rollupMoment || _moment;
 
-<p style="display: center">OpeingAmount = {{ opening$ }}</p>
-<table style="display: inline-block" id="t01">
-  <thead>
-    <th>IncomeID</th>
-    <th>TransactionDate</th>
-    <th>Amount</th>
-  </thead>
-  <tbody>
-    <tr *ngFor="let inc of reportI$">
-      <td>{{ inc.IncomeID }}</td>
-      <td>{{ inc.TransactionDate | date }}</td>
-      <td>{{ inc.Amount }}</td>
-    </tr>
-  </tbody>
-</table>
-<table id="t01" class="t01"> 
-  <thead>
-    <th>ExpenseID</th>
-    <th>TransactionDate</th>
-    <th>Amount</th>
-  </thead>
-  <tbody>
-    <tr *ngFor="let exp of reportE$">
-      <td>{{ exp.ExpenseID }}</td>
-      <td>{{ exp.TransactionDate | date }}</td>
-      <td>{{ exp.Amount }}</td>
-    </tr>
-  </tbody>
-</table>
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM-YYYY',
+  },
+  display: {
+    dateInput: 'MM-YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.scss'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
 
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
+})
+export class ReportComponent implements OnInit {
 
+  reportI$: any [];
+  reportE$: any [];
+  opening$: string;
+  date = new FormControl(moment());
 
-<!-- <input type="text" (keydown)="search($event)" placeholder="search total"> -->
+  constructor(private data: DataService) { }
 
-<!-- <label>Search: <input ng-model="searchText"></label>
-<table id="searchTextResults">
-  <tr><th>Name</th><th>Phone</th></tr>
-  <tr ng-repeat="friend in friends | filter:searchText">
-    <td>{{friend.name}}</td>
-    <td>{{friend.phone}}</td>
-  </tr>
-</table>
-<hr>
-<label>Any: <input ng-model="search.$"></label> <br>
-<label>Name only <input ng-model="search.name"></label><br>
-<label>Phone only <input ng-model="search.phone"></label><br>
-<label>Equality <input type="checkbox" ng-model="strict"></label><br>
-<table id="searchObjResults">
-  <tr><th>Name</th><th>Phone</th></tr>
-  <tr ng-repeat="friendObj in friends | filter:search:strict">
-    <td>{{friendObj.name}}</td>
-    <td>{{friendObj.phone}}</td>
-  </tr>
-</table> -->
+  ngOnInit() {
+  }
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+
+  chosenYearHandler(normalizedYear: Moment) {
+    const ctrlValue = this.date.value;
+    ctrlValue.year(normalizedYear.year());
+    this.date.setValue(ctrlValue);
+  }
+
+  chosenMonthHandler(normlizedMonth: Moment, datepicker: MatDatepicker<Moment>) {
+    const ctrlValue = this.date.value;
+    ctrlValue.month(normlizedMonth.month());
+    this.date.setValue(ctrlValue);
+    datepicker.close();
+  }
+
+  value: Date;
+   searchV(values: Date){
+     this.value = values;
+
+     this.data.reportTotal(values).subscribe(
+      (data: any) => {this.reportI$ = data.data, this.reportE$ = data.expense, this.opening$ = data.finalAmount;},
+      err => console.log(err),
+      () => console.log('done loading'),
+    );
+   }
+
+}
